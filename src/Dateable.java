8d9a9ef6b9734483f5edb1c3642a1250c25872bf
DoroDore <tlyz82@student.cis.edu.hk>
2023-08-31 14:55:55 +0800

    Updated the Lab and Completed It!

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        float minDateAge;
+        System.out.println("Would you like to know what's the lowest age you can date someone according to folk rules?\nThen enter your age below:");
+        double age = scanner.nextDouble();
+        minDateAge = (float) (age/2+7);
+        System.out.println(age + " year olds should date somebody who is at least " + minDateAge + " years old");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,37 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Pages {
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int minPages;
+        int age = -1;
+        boolean validAnswer = false;
+        System.out.println("There is a folk rule stating the number of pages you should read before giving up on a book is 100 - your age. Do you want to know how many pages you should read?\nThen enter your age below:");
+        while (!validAnswer) {
+            try{
+                age = scanner.nextInt();
+                if (age > 0) {
+                    validAnswer = true;
+                }
+                /*else {
+                    System.out.println("Please input a valid age.");
+                    scanner.nextLine();
+                }*/
+                throw new InputMismatchException();
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Please input a valid age.");
+                scanner.nextLine();
+            }
 
+        }
+        if (age > 100) {
+            System.out.println("Your age exceeds 100. That means, you are either:\n1. Very old\n2. Trolling me.\nEither way, you're probably too illiterate to read a book.");
+        }
+        else {
+            minPages = 100 - age;
+            System.out.println(age + " year olds should read at least " + minPages + " pages before giving up on a book");
+        }
     }
 }
